bdd3fa50875a5abc542eb8c5be00e8abb2404c7a
Katy Rosli <katyrosli@gmail.com>
2023-04-14 08:44:01 +0200

    manage to display the tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolsComponent } from './components/tools/tools.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,7 @@ import { ToolsComponent } from './components/tools/tools.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
